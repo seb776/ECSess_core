b59dd52ed797007023456dbb50a85d18a2a34ce9
z0rg <z0rg@localhost.localdomain>
2015-01-23 02:19:30 +0100

    removed build folder

--- build/CMakeFiles/ECSess_static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "bin/lib/libECSess_static.pdb"
-  "bin/lib/libECSess_static.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/ECSess_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/ECSess_static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "bin/lib/libECSess_static.a"
-)
